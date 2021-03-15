d00faf820a16de7124d831147e10c2d67a299da0
wangjy <490451981@qq.com>
2021-03-15 23:10:45 +0800

    commit

--- src/test/java/com/project/MyApplicationTests.java
+++ src/test/java/com/project/MyApplicationTests.java
@@ -1,47 +1,7 @@
 package com.project;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebAppConfiguration
 @SpringBootTest
-@Slf4j
-@RunWith(SpringRunner.class)
-//@MapperScan("com.project.mapper")
 public class MyApplicationTests {
-    private MockMvc mockMvc;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Before
-    public void before() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .alwaysExpect(status().isOk())
-                .alwaysDo(print())
-                .build();
-    }
-
-    @Test
-    public void test() throws Exception {
-        mockMvc.perform(get("/ping"));
-    }
 }
