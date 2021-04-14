5bafe416e51eb0e625a991e3228bf76f981a504c
Jiacheng-Gao <2889886695@qq.com>
2021-04-14 19:38:01 +0800

    数据导出弄完了,返回的是文件

--- src/test/java/com/project/MyApplicationTests.java
+++ src/test/java/com/project/MyApplicationTests.java
@@ -1,7 +1,24 @@
 package com.project;
 
+import com.project.utils.OutputUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
+@SpringBootTest(classes = MyApplication.class)
+@RunWith(SpringRunner.class)
 public class MyApplicationTests {
+
+    @Autowired
+    OutputUtils output;
+
+    @Test
+    public void test() throws Exception{
+
+
+    }
+
 }
