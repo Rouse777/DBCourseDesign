8bdd64c766589b21b3cb6190b2f4c7877d7bc9fe
Rouse777 <490451981@qq.com>
2021-04-20 20:59:32 +0800

    commit

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -8,7 +8,6 @@ import com.project.result.Result;
 import com.project.result.ResultCode;
 import com.project.service.UserService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
@@ -18,17 +17,21 @@ import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @Slf4j
 @Api(tags = "用户权限")
 public class UserController {
+    @Autowired
+    StringRedisTemplate redisTemplate;
     private UserService userService;
 
     @Autowired
@@ -36,36 +39,44 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Autowired
-    StringRedisTemplate redisTemplate;
-
     @PostMapping("/register")
     @ApiOperation(value = "普通用户注册")
     public Result register(@RequestBody @Valid AuthUser authUser) {
-            if(userService.getByName(authUser.getUsername())!=null)
-                return Result.success("注册失败，该用户已存在");
-            if(redisTemplate.opsForValue().get(authUser.getUsername())!=null)
-                return Result.success("请勿重复申请注册");
-            redisTemplate.opsForValue().set(authUser.getUsername(),authUser.getPassword());
-            return Result.success("注册申请已提交给管理员");
+        if (userService.getByName(authUser.getUsername()) != null)
+            return Result.success("注册失败，该用户已存在");
+        if (redisTemplate.opsForValue().get(authUser.getUsername()) != null)
+            return Result.success("请勿重复申请注册");
+        redisTemplate.opsForValue().set(authUser.getUsername(), authUser.getPassword());
+        return Result.success("注册申请已提交给管理员");
 
     }
 
+    @PostMapping("/super-register")
+    @ApiOperation(value = "测试用的注册，前端不要用此接口")
+    public Result superRegister(@RequestBody @Valid AuthUser authUser) {
+        //判断用户名是否已经存在，如果已存在，那就返回错误信息
+        if (userService.isNameExist(authUser.getUsername()))
+            return Result.fail("注册失败，该用户已存在");
+        if (userService.saveAuthUser(authUser)){
+            return Result.success("注册成功");
+        }
+        return Result.fail();
+    }
 
 
     @GetMapping("/admin/approval")
-    @ApiOperation(value="全部待审批的注册申请")
-    public Result approval(){
-        Set<String> a=redisTemplate.keys("*");
+    @ApiOperation(value = "全部待审批的注册申请")
+    public Result approval() {
+        Set<String> a = redisTemplate.keys("*");
         System.out.println(a);
-        List<AuthUser> ans=new ArrayList<AuthUser>();
-        for(String b:a){
-            ans.add(new AuthUser(b,redisTemplate.opsForValue().get(b).toString()));
+        List<AuthUser> ans = new ArrayList<AuthUser>();
+        for (String b : a) {
+            ans.add(new AuthUser(b, redisTemplate.opsForValue().get(b).toString()));
         }
 
-        Result result= Result.success();
-        HashMap<String,Object> ans1=new HashMap<>();
-        ans1.put("users",ans);
+        Result result = Result.success();
+        HashMap<String, Object> ans1 = new HashMap<>();
+        ans1.put("users", ans);
         System.out.println(ans1);
         result.setData(ans1);
         return result;
@@ -73,22 +84,19 @@ public class UserController {
 
 
     @PostMapping("/admin/registeruser")
-    @ApiOperation(value="前端返回某一用户的审批结果,pass为1表示通过审批为0反之(写在url上)")
-    public Result approval1(@RequestBody AuthUser authuser, @RequestParam String pass)throws Exception{
+    @ApiOperation(value = "前端返回某一用户的审批结果,pass为1表示通过审批为0反之(写在url上)")
+    public Result approval1(@RequestBody AuthUser authuser, @RequestParam String pass) throws Exception {
 
         redisTemplate.delete(authuser.getUsername());
-        if(pass.equals("1")){
-             if(userService.saveAuthUser(authuser))
-             return Result.success(authuser.getUsername()+"注册成功");
-             else return Result.success("注册失败该用户名已存在");
-        }
+        if (pass.equals("1")) {
+            if (userService.saveAuthUser(authuser))
+                return Result.success(authuser.getUsername() + "注册成功");
+            else return Result.success("注册失败该用户名已存在");
+        } else {
 
-        else{
-
-            return Result.success(authuser.getUsername()+"注册失败");
+            return Result.success(authuser.getUsername() + "注册失败");
         }
     }
-
 
 
     //shiro加持的登录方法，参数传进来一个UserDTO用户对象
@@ -96,10 +104,10 @@ public class UserController {
     @ApiOperation(value = "用户登录")
     public Result login(@RequestBody @Valid AuthUser authUser) {
         String msg;
-        UserPO ans=userService.getByName(authUser.getUsername());
+        UserPO ans = userService.getByName(authUser.getUsername());
         try {
             verifyUser(authUser);
-            return new Result(2000,"user",ans);
+            return new Result(2000, "user", ans);
         } catch (UnknownAccountException e) {
             msg = "用户" + authUser + "登录失败，用户名不存在";
         } catch (IncorrectCredentialsException e) {
@@ -109,11 +117,8 @@ public class UserController {
         }
         log.info(msg);
 
-        return new Result(2000,"user",ans);
+        return new Result(2000, "user", ans);
     }
-
-
-    
 
 
     private void verifyUser(AuthUser authUser) {
@@ -182,15 +187,15 @@ public class UserController {
     @PostMapping("/admin/user_delete")
     @ApiOperation(value = "删除用户", notes = "需要管理员权限，删除任意指定用户名的普通用户，不可删除管理员")
     public Result deleteUser(@RequestParam String username) {
-        System.out.println("delete"+username);
+        System.out.println("delete" + username);
         if (!userService.isNameExist(username)) {
             log.info("用户{}不存在，删除失败", username);
             return Result.fail("该用户不存在或已被删除");
         }
-        System.out.println("delete"+username);
+        System.out.println("delete" + username);
         String role = userService.getRoleByName(username);
-        System.out.println("delete:"+role);
-        System.out.println("delete"+username);
+        System.out.println("delete:" + role);
+        System.out.println("delete" + username);
         if (UserRole.ROLE_ADMIN.equals(role)) {
             log.info("用户{}为管理员，无法删除", username);
             return Result.fail("该用户为管理员，无法删除");
